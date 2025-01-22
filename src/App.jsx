f1bded89aebbaba0c85bf9b27b89065a659f2758
unknown <baxtiyor.gazimboyev@gmail.com>
2025-01-22 15:29:08 +0300

    madal

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Home from "./companent/home";
+import Home from "./companent/Home";
 import Qoshilish from "./companent/Qoshilish";
 
 function App() {
